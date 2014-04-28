7a042c30fcb6718a281daf9d21b7a12006a6fbe8
Sun PeiFeng <g7733pf@gmail.com>
2014-04-28 16:02:06 +0800

    Initial Commit

---
+++ Start&StopSchoolNetworkAccount/SFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SFAppDelegate.h
+//  Start&StopSchoolNetworkAccount
+//
+//  Created by 孙培峰 on 4/28/14.
+//  Copyright (c) 2014 孙培峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Start&StopSchoolNetworkAccount/SFViewController.h
@@ -0,0 +1,13 @@
+//
+//  SFViewController.h
+//  Start&StopSchoolNetworkAccount
+//
+//  Created by 孙培峰 on 4/28/14.
+//  Copyright (c) 2014 孙培峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFViewController : UIViewController
+
+@end
