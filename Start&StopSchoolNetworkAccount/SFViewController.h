f3a582a1b1f1e75589484c045600272bf10998a3
Sun PeiFeng <g7733pf@gmail.com>
2014-04-30 17:47:21 +0800

    成功登陆锐捷

--- Start&StopSchoolNetworkAccount/SFViewController.h
+++ Start&StopSchoolNetworkAccount/SFViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SFViewController : UIViewController<NSURLSessionDataDelegate>
+@interface SFViewController : UIViewController<NSURLSessionDataDelegate,UITextFieldDelegate>
 
 
 
--- Start&StopSchoolNetworkAccount/SFViewController.m
+++ Start&StopSchoolNetworkAccount/SFViewController.m
@@ -10,6 +10,9 @@
 
 @interface SFViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *verificationCodeImageView;
+@property (weak, nonatomic) IBOutlet UITextField *verificationCodeTextFieldView;
+@property (weak, nonatomic) IBOutlet UIButton *startLoginButton;
+@property (strong, nonatomic) NSString *verificationCode;
 
 @end
 
@@ -20,25 +23,26 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 
-
-    [self loadVerificationCode];
+    [self loadVerificationCodeImage];
+    
+    
 }
 
 
-- (void)loadVerificationCode
+- (void)loadVerificationCodeImage
 {
     NSURL *url = [NSURL URLWithString:@"https://whu-sb.whu.edu.cn:8443/selfservice/common/web/verifycode.jsp"];
     
     NSURLSessionConfiguration *defaultConfigObject = [NSURLSessionConfiguration defaultSessionConfiguration];
+    
     NSURLSession *defaultSession = [NSURLSession sessionWithConfiguration: defaultConfigObject delegate: self delegateQueue: [NSOperationQueue mainQueue]];
     
     NSURLSessionDataTask * dataTask = [defaultSession dataTaskWithURL:url
     completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
         if(error == nil)
         {
-            NSLog(@"Data: %@",data);
             _verificationCodeImageView.image = [UIImage imageWithData:data];
-            
+            NSLog(@"Load Verification Code Image Successfully!");
         }
         else
         {
@@ -59,15 +63,130 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)startLogin:(id)sender
+{
+    _verificationCode = [[NSString alloc]init];
+    _verificationCode = _verificationCodeTextFieldView.text;
+    [self login];
+}
 
-//    NSString *urlString = [NSString stringWithFormat:@"https://whu-sb.whu.edu.cn:8443/selfservice/common/web/verifycode.jsp"];
-//    NSURL *url = [NSURL URLWithString:urlString];
-//    NSMutableURLRequest *urlRequest = [[NSMutableURLRequest alloc]initWithURL:url];
-//
-//    [urlRequest setHTTPMethod:@"GET"];
-//    NSOperationQueue *operationQueue = [[NSOperationQueue alloc]init];
-//    [NSURLConnection sendAsynchronousRequest:urlRequest queue:operationQueue completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError)
-//     {
-//
-//     }];
+
+- (void)login
+{
+    NSURL *url = [NSURL URLWithString:@"https://whu-sb.whu.edu.cn:8443/selfservice/module/scgroup/web/login_judge.jsf"];
+    
+    NSURLSessionConfiguration *defaultConfigObject = [NSURLSessionConfiguration defaultSessionConfiguration];
+    
+    NSURLSession *defaultSession = [NSURLSession sessionWithConfiguration: defaultConfigObject delegate: self delegateQueue: nil];
+    NSMutableURLRequest * urlRequest = [NSMutableURLRequest requestWithURL:url];
+    NSString *params = [NSString stringWithFormat:@"act=add&name=2012301130125&password=204765&verify=%@",_verificationCode];
+    NSData *data = [params dataUsingEncoding:NSUnicodeStringEncoding];
+    [urlRequest setHTTPBody:data];
+    [urlRequest setHTTPMethod:@"POST"];
+    [urlRequest setHTTPBody:[params dataUsingEncoding:NSUTF8StringEncoding]];
+    [urlRequest setHTTPShouldHandleCookies:YES];
+
+    NSURLSessionDataTask * dataTask = [defaultSession dataTaskWithRequest:urlRequest completionHandler:^(NSData *data, NSURLResponse *response, NSError *error)
+       {
+           if(error == nil)
+           {
+               NSLog(@"1");
+               [self fetchURLUsingGETWithString:@"https://whu-sb.whu.edu.cn:8443/selfservice/module/userself/web/self_resume.jsf"];
+           }
+           else
+           {
+               NSLog(@"Error: %@", error);
+           }
+       }];
+    
+    [dataTask resume];
+
+
+}
+
+- (void)fetchURLUsingGETWithString:(NSString *)urlString
+{
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@",urlString]];
+    NSURLSessionConfiguration *defaultConfigObject = [NSURLSessionConfiguration defaultSessionConfiguration];
+    
+    NSURLSession *defaultSession = [NSURLSession sessionWithConfiguration: defaultConfigObject delegate: self delegateQueue: [NSOperationQueue mainQueue]];
+    NSMutableURLRequest * urlRequest = [NSMutableURLRequest requestWithURL:url];
+    [urlRequest setHTTPMethod:@"GET"];
+    [urlRequest setHTTPShouldHandleCookies:YES];
+    
+    NSURLSessionDataTask * dataTask = [defaultSession dataTaskWithRequest:urlRequest completionHandler:^(NSData *data, NSURLResponse *response, NSError *error)
+        {
+            if(error == nil)
+            {
+                NSLog(@"2");
+            }
+            else
+            {
+                NSLog(@"Error: %@", error);
+            }
+        }];
+    
+    [dataTask resume];
+
+}
+
+- (void)fetchAllSavedCookie
+{
+    NSHTTPCookieStorage *cookieStorge = [NSHTTPCookieStorage sharedHTTPCookieStorage];
+    for (NSHTTPCookie *cookie in [cookieStorge cookies]) {
+        NSLog(@"Once:%@", cookie);
+    }
+    
+    NSMutableDictionary *cookieProperties = [NSMutableDictionary dictionary];
+    [cookieProperties setObject:@"CookieName" forKey:NSHTTPCookieName];
+    [cookieProperties setObject:@"CookieValue" forKey:NSHTTPCookieValue];
+    [cookieProperties setObject:@"CookieDomain" forKey:NSHTTPCookieDomain];
+    [cookieProperties setObject:@"CookieOriginURL" forKey:NSHTTPCookieOriginURL];
+    [cookieProperties setObject:@"CookiePath" forKey:NSHTTPCookiePath];
+    [cookieProperties setObject:@"CookieVersion" forKey:NSHTTPCookieVersion];
+        
+//    NSHTTPCookie *cookie = [NSHTTPCookie cookieWithProperties:cookieProperties];
+//    [[NSHTTPCookieStorage sharedHTTPCookieStorage] setCookie:cookie];
+    
+    for (NSHTTPCookie *cookie in [cookieStorge cookies])
+    {
+        NSLog(@"Twice:%@", cookie);
+    }
+}
+
+//- (void)connection:(NSURLConnection *)connection // IN
+//    didReceiveData:(NSData *)data                // IN
+//{
+//    NSString *reply = [[NSString alloc] initWithData:data
+//                                            encoding:NSUTF8StringEncoding];
+//    NSNumber *statusInfo =[[reply JSONValue] valueForKey:@"status"] ;
+//    NSLog(@"reply=====%@",reply);
+//    [reply release];
+//    if ([statusInfo intValue]==1) {
+//        //保存cookie
+//        NSHTTPURLResponse *httpResponse=(NSHTTPURLResponse *)self.responseCopy;
+//        NSDictionary *fields = [httpResponse allHeaderFields ];
+//        NSLog(@"response头内容＝＝＝%@",[fields description]);
+//        //取得cookie
+//        if (self.cookie == nil) {
+//            NSString *tempCookie =[[NSString alloc] initWithString: [fields valueForKey:@"Set-Cookie"]];
+//            self.cookie=tempCookie;
+//            [tempCookie release];
+//            tempCookie=nil;
+//        }
+//        else{
+//            self.cookie = [fields valueForKey:@"Set-Cookie"];
+//        }
+//        
+//        NSLog(@"cookie = %@",self.cookie);
+//        //接受cookie
+//        [[NSHTTPCookieStorage sharedHTTPCookieStorage] setCookieAcceptPolicy:NSHTTPCookieAcceptPolicyAlways];
+//        NSLog(@"写入后:%@",[[NSHTTPCookieStorage sharedHTTPCookieStorage]cookies]);
+//    }
+
++ (NSStringEncoding)GBKStringEncoding
+{
+    return CFStringConvertEncodingToNSStringEncoding(kCFStringEncodingGB_18030_2000);
+}
+
 @end
